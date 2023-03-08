044c20d0973c3adbb4317d06cb7523f4d68540da
Nargis Habibi <habibin@oregonstate.edu>
2023-03-08 13:45:01 -0800

    forgot to add app.use(express.json()) app.use(express.urlencoded({extended: true})) app.use(express.static('public')) in app.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@
 var express = require('express');   // We are using the express library for the web server
 var app = express();            // We need to instantiate an express object to interact with the server in our code
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 PORT = 9861;                 // Set a port number at the top so it's easy to change in the future
 
